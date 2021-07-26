d9978377a2e9683736dc3acb47748a9991855005
MaxKliuba <klyubamaks@gmail.com>
2021-07-26 18:16:17 +0300

    project added

---
+++ app/src/main/java/com/maxclub/android/beatbox/BeatBoxActivity.java
@@ -0,0 +1,11 @@
+package com.maxclub.android.beatbox;
+
+import androidx.fragment.app.Fragment;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/com/maxclub/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,100 @@
+package com.maxclub.android.beatbox;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.maxclub.android.beatbox.databinding.FragmentBeatBoxBinding;
+import com.maxclub.android.beatbox.databinding.ListItemSoundBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class BeatBoxFragment extends Fragment {
+
+    private BeatBox mBeatBox;
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
+    }
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater,
+                             @Nullable @org.jetbrains.annotations.Nullable ViewGroup container,
+                             @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
+
+        return binding.getRoot();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private ListItemSoundBinding mBinding;
+
+        public SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+            mBinding.setViewModel(new SoundViewModel(mBeatBox));
+        }
+
+        public void bind(Sound sound) {
+            mBinding.getViewModel().setSound(sound);
+            mBinding.executePendingBindings();
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            setSounds(sounds);
+        }
+
+        @NonNull
+        @NotNull
+        @Override
+        public SoundHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            ListItemSoundBinding binding = DataBindingUtil.inflate(inflater,
+                    R.layout.list_item_sound, parent, false);
+
+            return new SoundHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull @NotNull BeatBoxFragment.SoundHolder holder, int position) {
+            Sound sound = mSounds.get(position);
+            holder.bind(sound);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mSounds.size();
+        }
+
+        public void setSounds(List<Sound> sounds) {
+            mSounds = sounds;
+        }
+    }
+}
